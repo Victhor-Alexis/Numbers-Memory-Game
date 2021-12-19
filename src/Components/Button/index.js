1aabda1865becd4c1ca83e5da654a6dfd3b2e55b
Victhor-Alexis <victhoralexis123@gmail.com>
2021-12-19 17:36:46 -0300

    CentralBoxPlaying funcionando, barra com evento de tempo também.

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container } from './styles';
 import { useDynimicityContext } from '../../Context/useDynimicityContext'
 
 const Button = ({children}) => {
-    const { whatToDisplay, btnValue } = useDynimicityContext();
+    const { whatToDisplay, btnValue, time } = useDynimicityContext();
+    const [timeoutId, setTimeoutId] = useState(0);
+        
+    const handleClick = () => {
+        if (timeoutId) {
+            console.log("Entrou!")
+            clearTimeout(timeoutId);
+            setTimeoutId(0);
+            return;
+        }
+        
+        const newTimeoutId = setTimeout(() => {
+            whatToDisplay("Stop");
+            clearTimeout(timeoutId);
+            setTimeoutId(0);
+        }, time*1000);
+        
+        setTimeoutId(newTimeoutId);
+    };
 
     return (
-        <Container onClick={() => {whatToDisplay(btnValue)}}>
+        <Container onClick={() => {
+                whatToDisplay(btnValue)
+
+                if (btnValue !== "Verify")
+                    handleClick()
+            }}>
             {children}
         </Container>
     );
